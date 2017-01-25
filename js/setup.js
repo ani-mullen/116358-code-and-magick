d393150a8306bb864711437b3438c8a7e0409e76
Anna Karpina <ani-mullen@yandex.ru>
2017-01-25 23:00:57 +0300

    Третье задание

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,57 @@
 
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var wizardCoat = setup.querySelector('#wizard-coat');
+var wizardEyes = setup.querySelector('#wizard-eyes');
+var setupFireball = setup.querySelector('.setup-fireball-wrap');
+
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var wizardEyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var setupFireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
 
 setupOpen.addEventListener('click', function () {
-  setup.classList.remove('.invisible');
+  setup.classList.remove('invisible');
 });
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('invisible');
+});
+
+wizardCoat.addEventListener('click', function () {
+  var colorCoatNumber = Math.floor(Math.random() * wizardCoatColors.length);
+  wizardCoat.style.fill = wizardCoatColors[colorCoatNumber];
+});
+
+wizardEyes.addEventListener('click', function () {
+  var colorEyesNumber = Math.floor(Math.random() * wizardEyesColors.length);
+  wizardEyes.style.fill = wizardEyesColors[colorEyesNumber];
+});
+
+setupFireball.addEventListener('click', function () {
+  var colorFireballNumber = Math.floor(Math.random() * setupFireballColors.length);
+  setupFireball.style.background = setupFireballColors[colorFireballNumber];
+});
+
+
